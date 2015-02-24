7a1c221bb284a38c7b6ac28f410082cffb66c8dd
David Gautier <david.gautier@ginko.io>
2015-02-24 18:14:11 +0100

    added : Fonts enum to have font name for Arial

---
+++ src/main/java/taro/spreadsheet/model/Fonts.java
@@ -0,0 +1,15 @@
+package taro.spreadsheet.model;
+
+/**
+ * Created by dgautier on 2/24/2015.
+ */
+public enum Fonts {
+
+    ARIAL("Arial");
+
+    private final String value;
+
+    Fonts(String value) {
+        this.value = value;
+    }
+}
